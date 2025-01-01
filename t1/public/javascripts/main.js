2079252fe932744e6c36663003a28ea5723ac289
Paulo <paulo.rn@estudante>
2025-01-01 16:12:40 -0300

    adding first version of pokedex project

---
+++ t1/public/javascripts/main.js
@@ -0,0 +1,48 @@
+function capitalizeFirstLetter(text) {
+    if (!text) return ''; // Verifica se o texto não está vazio
+    return text.charAt(0).toUpperCase() + text.slice(1);
+  }
+
+async function getPokemon(pokemonName){
+    pokemonName = pokemonName.toLowerCase();
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    const dt = response.data;
+    return dt;
+}
+
+function fillPokemonInfos(pokemon){
+    const pokeName = document.querySelector('.pokedex .name');
+    const pokeNumber = document.querySelector('.pokedex .number');
+    const pokeImg = document.querySelector('img.pokemon');
+    pokeName.textContent = capitalizeFirstLetter(pokemon.species.name);
+    pokeNumber.textContent = `Nº ${pokemon.id}`;
+    pokeImg.setAttribute('src', `${pokemon.sprites.front_default}`)
+}
+
+const searchField = document.querySelector('input')
+searchField.addEventListener('change', async (e) => {
+    try{
+        const pokemon = await getPokemon(e.target.value);
+        fillPokemonInfos(pokemon);
+    } catch(err){
+        const pokeName = document.querySelector('.pokedex .name');
+        const pokeNumber = document.querySelector('.pokedex .number');
+        const pokeImg = document.querySelector('img.pokemon');
+
+        //set values
+        pokeNumber.textContent= "";
+        pokeImg.setAttribute('src', ``);
+        pokeName.textContent = e.target.value == '' ?"" : "POKEMON INVÁLIDO, DIGITE UM NOME VÁLIDO";
+    }
+})
+// document.addEventListener('DOMContentLoaded', (e) => {
+//     console.log('hello')
+//     const pokeName = document.querySelector('.pokedex .name');
+//     const pokeNumber = document.querySelector('.pokedex .number');
+//     const pokeImg = document.querySelector('img .pokemon');
+//     pokeName.textContent = pokemon.species.name;
+//     pokeNumber.textContent = pokemon.id;
+//     console.log(pokemon.species.name)
+//     console.log(pokemon.id)
+//     pokeImg.setAttribute('src', `${pokemon.sprites.front_default}`)
+// })
